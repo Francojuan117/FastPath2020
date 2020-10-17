dc05ef7eb605c6fbda4417a3a1181e397a82d6d8
francojuan117 <francojuan117@gmail.com>
2020-10-16 17:06:42 -0700

    sect10ass1 stalker complete

---
+++ Section 10, JS Stalker Exercise Solution/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What your first name pal?");
+var lastName = prompt ("Whats ya last name?");
+var age = prompt("How about your age?");
+
+console.log("Hello their " + firstName + " " + lastName);
+console.log("So you are " + age + " years old. Wow! Thank you for being here. :)")
